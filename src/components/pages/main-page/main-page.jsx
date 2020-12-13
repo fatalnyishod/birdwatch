99044456df6c1ffa1734c688eb9520e62ae5696f
Mikhail Danilov <mfdanilovv@gmail.com>
2020-12-13 19:04:53 +0300

    Блок о главном

--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './banner/banner.jsx';
 import Filter from './filter/filter.jsx';
 import RoundUp from './round-up/round-up.jsx';
+import Foundation from './foundation/foundation.jsx';
 
 const MainPage = () => {
 	return (
@@ -11,6 +12,8 @@ const MainPage = () => {
 			<Filter/>
 
 			<RoundUp/>
+
+			<Foundation/>
 
 			<span>&nbsp;</span>
 		</>
