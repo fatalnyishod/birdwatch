466a16d9972a9b36c383cd4e3136c803aa7e6972
Mikhail Danilov <mfdanilovv@gmail.com>
2020-12-13 15:15:51 +0300

    Сводка статей

--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './banner/banner.jsx';
 import Filter from './filter/filter.jsx';
+import RoundUp from './round-up/round-up.jsx';
 
 const MainPage = () => {
 	return (
@@ -9,23 +10,7 @@ const MainPage = () => {
 
 			<Filter/>
 
-			<div>
-				<h1 className="title title_h1">Заголовок первого уровня</h1>
-				<h2 className="title title_h2">Заголовок второго уровня</h2>
-				<h3 className="title title_h3">Заголовок третьего уровня</h3>
-				<h4 className="title title_h4">Заголовок четвертого уровня</h4>
-
-				<blockquote>
-					<p className="text text_quote">Цитата.</p>
-				</blockquote>
-
-				<p className="text">Текст.</p>
-				<p className="text text_b">Жирный текст.</p>
-				<p className="text text_s">Подпись.</p>
-				<p className="text text_s text_b">Подпись жирным.</p>
-
-				<a href="#" className="link">Ссылка</a>
-			</div>
+			<RoundUp/>
 		</>
 	);
 };
