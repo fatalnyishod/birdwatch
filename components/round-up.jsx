e93cc383b51f9e09f2615c34fe2f8461cfe77e08
Marat Magomedov <m.magomedov@telphin.ru>
2021-01-11 21:59:06 +0300

    [WIP]

--- components/round-up.jsx
+++ components/round-up.jsx
@@ -1,82 +1,84 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const RoundUp = () => {
-	return (
-		<div className="round-up">
-			<div className="grid">
-				<h4 className="title title_h4">Последние записи</h4>
-			</div>
-			<margin-16 />
+  return (
+    <div className='round-up'>
+      <div className='grid'>
+        <h4 className='title title_h4'>Последние записи</h4>
+      </div>
+      <margin-16 />
 
-			<div className="grid round-up__grid">
-				<Link href="/">
-					<a className="round-up__item">
-						<img src="https://i.pinimg.com/originals/7b/a6/94/7ba694ed1fd5966886c1eb79dfcddaf5.jpg" className="round-up__img" />
-						<span className="text text_light">Марат Магомедов, 13 декабря 2020</span>
-						<p className="text text_light">Бинокль — главное оружие бёрдера, его нужно выбирать с умом. Рассказываем, как это сделать правильно.</p>
-					</a>
-				</Link>
+      <div className='grid round-up__grid'>
+        <Link href='/blog/binokulars-and-other-optics'>
+          <a className='round-up__item'>
+            <div className='round-up__item-wrap'>
+              <img
+                src='https://i.pinimg.com/originals/7b/a6/94/7ba694ed1fd5966886c1eb79dfcddaf5.jpg'
+                className='round-up__img'
+              />
+              <h3 className='title title_h3 title_white'>
+                Бинокли, подзорные трубы и другая оптика
+              </h3>
+            </div>
+            <span className='text text_light'>Марат Магомедов, 12 января</span>
+            <p className='text'>
+              Бинокль — главное оружие бёрдера, его нужно выбирать с умом. Рассказываем, как это
+              сделать правильно.
+            </p>
+          </a>
+        </Link>
 
-				<Link href="/">
-					<a className="round-up__item">
-						<img src="https://nnst1.gismeteo.ru/images/2018/12/1860bc77.jpg" className="round-up__img" />
-						<span className="text text_light">Вадим Злыгастев, 12 декабря 2020</span>
-						<p className="text text_light">Руководство по определению птиц и полевым определителям.</p>
-					</a>
-				</Link>
+        <Link href='/blog/find-your-guide'>
+          <a className='round-up__item'>
+            <div className='round-up__item-wrap'>
+              <img
+                src='https://nnst1.gismeteo.ru/images/2018/12/1860bc77.jpg'
+                className='round-up__img'
+              />
+              <h3 className='title title_h3 title_white'>Определите свой определитель</h3>
+            </div>
+            <span className='text text_light'>Екатерина Сичинава, 12 января</span>
+            <p className='text'>Руководство по многообразию полевых определителей.</p>
+          </a>
+        </Link>
 
-				<Link href="/">
-					<a className="round-up__item">
-						<img src="https://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2020-03/RIAN_2824702.HR_.ru_.jpg?itok=YEoU8tHn" className="round-up__img" />
-						<span className="text text_light">Михаил Данилов, 11 декабря 2020</span>
-						<p className="text text_light">Лайфлист как главная гордость и сокровище бёрдвотчера. Рассказываем, где лучше вести полевые дневники, чеклисты и публиковать фотографии. И как правильно это делать.</p>
-					</a>
-				</Link>
+        <Link href='/blog/where-is-your-lifelist'>
+          <a className='round-up__item'>
+            <div className='round-up__item-wrap'>
+              <img
+                src='https://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2020-03/RIAN_2824702.HR_.ru_.jpg?itok=YEoU8tHn'
+                className='round-up__img'
+              />
+              <h3 className='title title_h3 title_white'>
+                Записывайте свои наблюдения, они нужны сообществу бёрдеров и ученым-орнитологам
+              </h3>
+            </div>
+            <span className='text text_light'>Анастасия Бесфамильная, 12 января</span>
+            <p className='text'>
+              Лайфлист как главная гордость и сокровище бёрдвотчера. Рассказываем, где лучше вести
+              полевые дневники, чеклисты и публиковать фотографии. И как правильно это делать.
+            </p>
+          </a>
+        </Link>
 
-				<Link href="/">
-					<a className="round-up__item">
-						<img src="https://tayga.info/media/images/news/159/159949/thumb.jpg" className="round-up__img" />
-						<span className="text text_light">Марат Магомедов, 10 декабря 2020</span>
-						<p className="text text_light">Единомышленники, наставники и друзья в орнитологии — где искать экскурсии и туры в своем городе.</p>
-					</a>
-				</Link>
-			</div>
-
-			<div className="grid round-up__grid">
-				<Link href="/">
-					<a className="round-up__item">
-						<img src="https://tvzvezda.ru/storage/news_other_images/2019/12/15/45ff7331ffe645f2a78f2c55fa918413.jpg" className="round-up__img" />
-						<span className="text text_light">Михаил Данилов, 9 декабря 2020</span>
-						<p className="text text_light">Langtang National Park — Langtang Village to Kyanjin Gumba, Rasuwa, Madhyamanchal, Nepal</p>
-					</a>
-				</Link>
-
-				<Link href="/">
-					<a className="round-up__item">
-						<img src="https://img6.eadaily.com/c650x400/o/541/9882205265e7a8f54b48339a0bf88.jpeg" className="round-up__img" />
-						<span className="text text_light">Вадим Злыгастев, 8 декабря 2020</span>
-						<p className="text text_light">Langtang National Park — Langtang Village to Kyanjin Gumba, Rasuwa, Madhyamanchal, Nepal</p>
-					</a>
-				</Link>
-
-				<Link href="/">
-					<a className="round-up__item">
-						<img src="https://im.kommersant.ru/Issues.photo/OGONIOK/2019/043/KMO_085979_08045_1_t218_222727.jpg" className="round-up__img" />
-						<span className="text text_light">Вадим Злыгастев, 7 декабря 2020</span>
-						<p className="text text_light">Langtang National Park — Langtang Village to Kyanjin Gumba, Rasuwa, Madhyamanchal, Nepal</p>
-					</a>
-				</Link>
-
-				<Link href="/">
-					<a className="round-up__item">
-						<img src="https://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2019-11/20190124_gaf_uw8_402.jpg?itok=M5zf0W24" className="round-up__img" />
-						<span className="text text_light">Марат Магомедов, 6 декабря 2020</span>
-						<p className="text text_light">Langtang National Park — Langtang Village to Kyanjin Gumba, Rasuwa, Madhyamanchal, Nepal</p>
-					</a>
-				</Link>
-			</div>
-		</div>
-	);
+        <Link href='/blog/more-eyes-more-birds'>
+          <a className='round-up__item'>
+            <div className='round-up__item-wrap'>
+              <img
+                src='https://tayga.info/media/images/news/159/159949/thumb.jpg'
+                className='round-up__img'
+              />
+              <h3 className='title title_h3 title_white'>Чем больше глаз — тем больше птиц</h3>
+            </div>
+            <span className='text text_light'>Анастасия Бесфамильная, 12 января</span>
+            <p className='text'>
+              Единомышленники, наставники и друзья в орнитологии — где искать экскурсии и туры.
+            </p>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default RoundUp;
