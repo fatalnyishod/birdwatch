bc694f1e78e1c03f1d451931fce84ac416a165ce
Marat Magomedov <m.magomedov@telphin.ru>
2021-02-23 00:51:39 +0300

    первая статья

--- components/banner.jsx
+++ components/banner.jsx
@@ -1,37 +1,39 @@
-import Header from "./header";
-import Link from "next/link";
+import Header from './header';
+import Link from 'next/link';
 
 const Banner = () => {
-	return (
-		<div className="banner">
-			<div className="banner__overlay"></div>
+  return (
+    <div className='banner'>
+      <div className='banner__overlay'></div>
 
-			<Header isBanner={true} />
+      <Header isBanner={true} />
 
-			<div className="grid banner__grid">
-				<div className="banner__wrap">
-					<h1 className="title title_h1 banner__title">
-						Бёрдвотчинг —
-						<br />
-						что это и как начать?
-					</h1>
-					<margin-24/>
+      <div className='grid banner__grid'>
+        <div className='banner__wrap'>
+          <h1 className='title title_h1 banner__title'>
+            Бёрдвотчинг —
+            <br />
+            что это и как начать?
+          </h1>
+          <margin-24 />
 
-					<p className="text text_b text_white">
-						Видели людей, которые бегают по парку с биноклями и фотоаппаратами и что-то высматривают в кроне деревьев? Это бёрдвотчеры — любители птиц. Они коллекционируют встреченные виды и помогают развивать науку.
-					</p>
-					<margin-24/>
+          <p className='text text_b text_white'>
+            Видели людей, которые бегают по парку с биноклями и фотоаппаратами и что-то высматривают
+            в кроне деревьев? Это бёрдвотчеры — любители птиц. Они коллекционируют встреченные виды
+            и помогают развивать науку.
+          </p>
+          <margin-24 />
 
-					<p className="text text_b text_white">Вы тоже можете попробовать, это затягивает.</p>
-					<margin-24/>
+          <p className='text text_b text_white'>Вы тоже можете попробовать, это затягивает.</p>
+          <margin-24 />
 
-					<Link href="/blog/birdwatching-what-it-is-and-how-to-start">
-						<a className="btn btn_white">Подробнее о бёрдвотчинге</a>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+          <Link href='/blog/birdwatching-what-it-is-and-how-to-start'>
+            <a className='btn btn_white'>Подробнее о бёрдвотчинге</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
